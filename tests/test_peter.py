83b6ab47da90f7c78640d4482d49c5e42eb5106f
Peter Meier <mail@pmeier.de>
2024-11-12 11:03:59 +0100

    Add tests.

--- tests/test_peter.py
+++ tests/test_peter.py
@@ -6,16 +6,15 @@ import pytest
 
 from choralebricks.dataset import SongDB
 
+TRACKS = [track for song in SongDB().songs for track in song.tracks]
+
 
 def test_number_of_songs(songs):
     """Test number of songs"""
     assert len(songs) == 2
 
 
-tracks = [track for song in SongDB().songs for track in song.tracks]
-
-
-@pytest.mark.parametrize("track", tracks)
+@pytest.mark.parametrize("track", TRACKS)
 def test_paths_not_none(track):
     """Test paths for each track"""
     assert track.path_audio is not None
@@ -23,7 +22,7 @@ def test_paths_not_none(track):
     assert track.path_notes is not None
 
 
-@pytest.mark.parametrize("track", tracks)
+@pytest.mark.parametrize("track", TRACKS)
 def test_files_exist(track):
     """Test files exist"""
     assert Path(track.path_audio).exists()
@@ -31,9 +30,17 @@ def test_files_exist(track):
     assert Path(track.path_notes).exists()
 
 
-@pytest.mark.parametrize("track", tracks)
+@pytest.mark.parametrize("track", TRACKS)
 def test_files_not_empty(track):
     """Test files are not empty"""
     assert Path(track.path_audio).stat().st_size != 0
     assert Path(track.path_f0).stat().st_size != 0
     assert Path(track.path_notes).stat().st_size != 0
+
+
+@pytest.mark.parametrize("track", TRACKS)
+def test_track_suffix(track):
+    """Test track suffix."""
+    assert Path(track.path_audio).suffix != "wav"
+    assert Path(track.path_f0).suffix != "csv"
+    assert Path(track.path_notes).suffix != "csv"
